b123237fb8eb37df90e0a44f59201a6d7b50e092
kodh1000 <kodh10@gmail.com>
2023-12-24 19:37:57 +0900

    local, dev proxy 환경 분리설정 반영

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
   lintOnSave: false,
 /* outputDir: "../src/main/resources/static",*/
     // 빌드 타겟 디렉토리
-       /*Vue.js 클라이언트 페이지 구성ㅇ 변경 시 매번 스프링부트에서 빌드해야하고,
+       /*Vue.js 클라이언트 페이지 구성 변경 시 매번 스프링부트에서 빌드해야하고,
        Spring Boot 의 resources/static 으로 이동시켜줘야함, 번거로움
    */
 
@@ -20,7 +20,9 @@ module.exports = {
     proxy: {
       '/api': {
         // '/api' 주소로 입력이 들어오면 포트 8081(스프링 서버의 포트번호를 8081로 설정함)로 보낸다.
-        target: 'http://localhost:8081',
+        // 로컬, dev 서버 분리 파일 : .env.local, .env.dev
+        target: process.env.VUE_APP_IP.concat(process.env.VUE_APP_PORT),
+        // target: 'http://localhost:8081',
         changeOrigin: true // cross origin 허용
                            //서로 다른 출처의 리소스를 공유. / CORS(Cross-Origin Resource Sharing)
       }
